eaca419a0e19f6eac7b9a454e5d781cc5b983f7a
Jap Purohit <67332925+jappurohit041@users.noreply.github.com>
2024-08-28 19:18:15 +0000

    feat(point-5): :sparkles: modifed the myfile: added the validation, function typedef and handled exception

--- myfile.py
+++ myfile.py
@@ -1,8 +1,22 @@
-amount = float(input("Enter the amount: "))
+def calculate_tax(amount, tax_rate):
+    """
+    calculate_tax function calculates the tax
 
-if amount < 0:
-    print("Invalid amount")
-else:
-    tax = amount * 0.0725
-    total_amount = amount + tax
-    print(f"Total amount after adding 7.25% tax: {total_amount}")
+    :param amount: enter the positive float value great than 0
+    :param tax_rate: pass the tax rate in the percentage
+    :return: it returns the total amount including the tax 
+    """
+    try: 
+        if amount < 0:
+            raise ValueError("Invalid amount: Not allowed to enter less than 0")
+        elif tax_rate < 0:
+            raise ValueError("Invalid tax rate: Not allowed to enter less than 0%")
+        else:
+            tax = amount * (tax_rate / 100)
+            total_amount = amount + tax
+            print(f"Total amount after adding {tax_rate}% tax: {total_amount}")
+            return total_amount
+    except ValueError as e:
+        raise e
+    except Exception as e:
+        raise e
