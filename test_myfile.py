005aa754faff9ca79ab958ff5d1c2b2da8418e42
Jap Purohit <67332925+jappurohit041@users.noreply.github.com>
2024-08-28 22:48:11 +0000

    test(point-10): :white_check_mark: updated the test to fail at one single step

--- test_myfile.py
+++ test_myfile.py
@@ -6,10 +6,4 @@ def test_passing():
 
 
 def test_failing():
-    try:
-        # This raises a ValueError
-        calculate_tax(-100, 7.25)  
-    except ValueError as e:
-        assert str(e) == "Invalid Amount: Not allowed to enter less than 0"
-    else:
-        assert False, "ValueError was not raised for a negative amount"
+    calculate_tax(-100, 7.25)  
